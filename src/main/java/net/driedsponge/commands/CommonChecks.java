e431abe104b68a6b25a2d7cec19ccef479108b46
DriedSponge <jordan@driedsponge.net>
2021-11-29 20:51:09 -0800

    Call check on skip

--- src/main/java/net/driedsponge/commands/CommonChecks.java
+++ src/main/java/net/driedsponge/commands/CommonChecks.java
@@ -1,6 +1,7 @@
 package net.driedsponge.commands;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 
@@ -12,5 +13,15 @@ public class CommonChecks {
      */
     public static boolean playingMusic(Guild guild){
         return guild.getAudioManager().isConnected() && Play.PLAYERS.get(guild) != null;
+    }
+
+    /**
+     * Check if the member is in the same call as the bot
+     * @param member The member to check
+     * @param guild The server to check
+     * @return Whether the member is listening to music or not
+     */
+    public static boolean listeningMusic(Member member, Guild guild){
+        return (member.getVoiceState().inVoiceChannel() && member.getVoiceState().getChannel() == guild.getAudioManager().getConnectedChannel());
     }
 }
--- src/main/java/net/driedsponge/commands/Skip.java
+++ src/main/java/net/driedsponge/commands/Skip.java
@@ -1,6 +1,5 @@
 package net.driedsponge.commands;
 
-import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import net.driedsponge.VoiceController;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -9,6 +8,10 @@ public class Skip extends ListenerAdapter {
     @Override
     public void onSlashCommand(SlashCommandEvent event) {
         if (!event.getName().equals("skip")) return;
+        if(!CommonChecks.listeningMusic(event.getMember(),event.getGuild())){
+            event.reply("You need to be in a voice channel with the bot to skip.").setEphemeral(true).queue();
+            return;
+        }
         if(CommonChecks.playingMusic(event.getGuild())){
             VoiceController vc = Play.PLAYERS.get(event.getGuild());
             event.reply(":fast_forward: Skipping **"+vc.getNowPlaying().getInfo().title+"**").queue();
