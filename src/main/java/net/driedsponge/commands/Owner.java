46ba76632d10812fa053644b73f320ba3f4d7982
DriedSponge <jordan@driedsponge.net>
2021-11-30 15:59:31 -0800

    Added some more usefull commands

--- src/main/java/net/driedsponge/commands/Owner.java
+++ src/main/java/net/driedsponge/commands/Owner.java
@@ -1,22 +1,55 @@
 package net.driedsponge.commands;
 
 import net.driedsponge.Main;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 
-public class Owner extends ListenerAdapter {
+import java.awt.*;
 
+public class Owner extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
         if(!event.getAuthor().getId().equals(Main.OWNER_ID)) return;
         if(event.getMessage().getContentRaw().startsWith("!initialize")){
             initialize(event.getJDA());
             event.getMessage().reply("Initializing slash commands. May take up to one hour.").queue();
+        }else if(event.getMessage().getContentRaw().startsWith("!statistics")){
+            statistics(event);
+        }else if(event.getMessage().getContentRaw().startsWith("!guildlist")){
+            guildList(event);
         }
     }
+
+    private void guildList(MessageReceivedEvent event){
+        JDA jda = event.getJDA();
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setColor(Color.CYAN);
+        embedBuilder.setTitle(jda.getSelfUser().getName() + " Guilds");
+        StringBuilder builder = new StringBuilder();
+        builder.append("```");
+        jda.getGuilds().forEach(guild -> {
+            builder.append("\n ").append(guild.getName()).append(" (").append(guild.getId()).append(")");
+        });
+        builder.append("\n```");
+        embedBuilder.setDescription(builder.toString());
+        event.getMessage().replyEmbeds(embedBuilder.build()).queue();
+    }
+
+    private void statistics(MessageReceivedEvent event){
+        JDA jda = event.getJDA();
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setColor(Color.CYAN);
+        embedBuilder.setTitle(jda.getSelfUser().getName() + " Statistics");
+        embedBuilder.addField("Guilds",String.valueOf(jda.getGuilds().size()),true);
+        embedBuilder.addField("Currently Entertaining",String.valueOf(Play.PLAYERS.size()),true);
+        event.getMessage().replyEmbeds(embedBuilder.build()).queue();
+    }
+
     private void initialize(JDA jda){
         jda.upsertCommand("ping","Check the bots ping.").queue(); // Implemented
         jda.upsertCommand("leave","Tells the bot to leave the current voice channel.").queue(); // Implemented
