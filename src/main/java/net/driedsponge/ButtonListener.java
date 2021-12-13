cf72f65999a12fa38e4306044e36240f40e20d3a
DriedSponge <jordan@driedsponge.net>
2021-12-12 21:45:54 -0800

    BUTTON

--- src/main/java/net/driedsponge/ButtonListener.java
+++ src/main/java/net/driedsponge/ButtonListener.java
@@ -10,7 +10,9 @@ public class ButtonListener extends ListenerAdapter {
     @Override
     public void onButtonClick(@Nonnull ButtonClickEvent event) {
         if(event.getButton().getId().equals("guildlist")) {
-            event.replyEmbeds(Owner.guildList(event.getJDA()).build()).queue();
+            event.replyEmbeds(Owner.guildList(event.getJDA()).build())
+                    .addActionRow(Owner.CALL_LIST_BUTTON)
+                    .queue();
         }else if(event.getButton().getId().equals("entertaining")){
             event.replyEmbeds(Owner.callList(event.getJDA()).build()).queue();
         }
