955642f0752ad0f9fdd5c44fb000e6cf9016f6c4
jordan <jordan@driedsponge.net>
2023-12-24 01:59:17 -0800

    Log bug reports

--- src/main/java/net/driedsponge/commands/util/Bug.java
+++ src/main/java/net/driedsponge/commands/util/Bug.java
@@ -1,13 +1,17 @@
 package net.driedsponge.commands.util;
 
 import net.driedsponge.Main;
+import net.driedsponge.VoiceController;
 import net.driedsponge.commands.SlashCommand;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.awt.*;
 
 public class Bug extends SlashCommand {
+    private static final Logger logger = LoggerFactory.getLogger(Bug.class);
 
     public Bug() {
         super("bug");
@@ -22,7 +26,7 @@ public class Bug extends SlashCommand {
         embedBuilder.addField("Guild",event.getGuild().getName()+" ("+event.getGuild().getId()+")", true);
         embedBuilder.setDescription(event.getOptions().get(0).getAsString());
         embedBuilder.setAuthor(event.getUser().getName(),event.getUser().getAvatarUrl());
-
+        logger.warn("Bug Report From "+event.getUser().getName()+" ("+event.getUser().getId()+") in "+event.getGuild().getName()+" ("+event.getGuild().getId()+")"+": "+event.getOptions().get(0).getAsString());
         event.getJDA().retrieveUserById(Main.OWNER_ID).queue(owner ->{
             owner.openPrivateChannel().queue(response ->{
                 response.sendMessageEmbeds(embedBuilder.build()).queue();
