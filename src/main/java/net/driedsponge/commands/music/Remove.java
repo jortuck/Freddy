2cbecb81fa4a43f502deb48bb219e1cbc99dfe27
Jordan <jordan@driedsponge.net>
2024-06-22 19:45:07 -0700

    Final remove

--- src/main/java/net/driedsponge/commands/music/Remove.java
+++ src/main/java/net/driedsponge/commands/music/Remove.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
-public class Remove extends SlashCommand {
+public final class Remove extends SlashCommand {
     public Remove() {
         super("remove");
     }
