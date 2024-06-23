31564e6d03320ea9a0e016aab3f9f961b82e023b
Jordan <jordan@driedsponge.net>
2024-06-22 19:45:45 -0700

    Make MessageListener class final

--- src/main/java/net/driedsponge/MessageListener.java
+++ src/main/java/net/driedsponge/MessageListener.java
@@ -8,7 +8,7 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class MessageListener extends ListenerAdapter {
+public final class MessageListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
         if(event.getAuthor().isBot()) return;
